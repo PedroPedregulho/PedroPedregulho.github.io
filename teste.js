a54b631d03c985f86fca4539fea073ab81e29195
PedroPedregulho <79160618+PedroPedregulho@users.noreply.github.com>
2021-02-20 23:39:59 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,41 @@
+var sla=document.querySelector('#aaa')
+var slaa=document.querySelector('#aa')
+var h1=document.querySelector('h1')
+var h2=document.querySelector('h2')
+var h3=document.querySelector('h3')
+var contador=0
+var maior=0
+
+maior=localStorage.maior
+sla.addEventListener('click',mover)
+slaa.addEventListener('click',alerta)
+
+function mover(){
+    sla.style.left=Math.random()*500+1+ 'px'
+    sla.style.top=Math.random()*500+1+ 'px'
+    sla.style.width=Math.random()*200+1 +'px'
+    sla.style.height=sla.style.width
+    contador++
+}
+function alerta(){
+    alert('Perdeu')
+    contador=0
+}
+
+
+setInterval(() => {
+    h1.innerHTML='Pontos: ' + contador
+    h2.innerHTML='Sua Maior Pontuaçao: ' + maior
+    h3.innerHTML='Maior de todos-Pedro:92'
+    localStorage.maior=maior
+    if(contador>maior){
+        maior=contador
+    }
+}, 000);
+
+
+
+if(localStorage.sla1!=1){
+    var maior=0
+}
+localStorage.setItem('sla1',1)
